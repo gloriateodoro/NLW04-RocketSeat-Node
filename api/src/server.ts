781f02d02e3a8be25ea25f80da0d57b8addb7c7c
gloriateodoro <gloriateodoro1@gmail.com>
2021-02-24 20:43:37 -0300

    servidor

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,27 +1,11 @@
+import 'reflect-metadata';
 import express from 'express';
+import "./database";
+import { router } from "./routes"
 
 const app = express();
 
-/*
- *GET => Busca
- *POST => Salvar
- *PUT => Alterar
- *Delete => Deletar
- *PATCH => Alteração específica
- */
+app.use(express.json());
+app.use(router);
 
-
- //http://localhost:3333/users
-app.get("/users", (request, response) => {
-    return response.json({message: "Hello World - NLW04"});
-})
-
-// 1 param => Rota(Recurso, API)
-// 2 param => request, response
-
-app.post("/", (request, response) => {
-    //Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-})
-
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3030, () => console.log("Server is running!"));
